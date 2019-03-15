08f452351a1a393b1f0745201e020ea853824898
Marcus Vinicius da Silva Borges <marcus.f22@gmail.com>
2019-03-15 14:39:04 -0300

    Updates Items views and controller display/accept a item's photo.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -58,6 +58,6 @@ class ItemsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def item_params
-      params.require(:item).permit(:name, :description, :price)
+      params.require(:item).permit(:name, :description, :price, :photo)
     end
 end

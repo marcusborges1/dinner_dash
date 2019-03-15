abf14486324edd573fd86fe5b9d66f1b721de087
Marcus Vinicius da Silva Borges <marcus.f22@gmail.com>
2019-03-15 15:36:20 -0300

    Creates Item attributes validations for name, description and price.

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,7 @@
 class Item < ApplicationRecord
   has_one_attached :photo
+  
+  validates :name, presence: true, uniqueness: true
+  validates :description, presence: true
+  validates :price, presence: true, numericality: { greater_than: 0 }
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to validate_presence_of(:name) }
+  it { is_expected.to validate_uniqueness_of(:name) }
+  it { is_expected.to validate_presence_of(:description) }
+  it { is_expected.to validate_presence_of(:price) }
+  it { is_expected.to validate_numericality_of(:price).is_greater_than(0) }
 end
